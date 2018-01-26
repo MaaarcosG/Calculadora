ecbed0b1d28e31fa4b3d9c7d285a045d706d7cd3
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-26 11:08:03 -0600

    Interfaz

--- src/Interfaz_Calculadora.java
+++ src/Interfaz_Calculadora.java
@@ -1,4 +1,10 @@
-
+/**
+ * @author Marcos Gutierrez				17909
+ * @author Raul Monzon					17014
+ * @since 26 enero 2018	
+ * @Propuse Es una calculadora que lee un archivo de texto
+ *
+ */
 public interface Interfaz_Calculadora {
 
 }
