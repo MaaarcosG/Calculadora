80150a55859adcefc35a1c86bf2e94f567c221fa
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-26 11:45:05 -0600

    Interfaz de la calculadora

---
+++ src/CalculadoraI.java
@@ -0,0 +1,8 @@
+/**
+ * @author Marcos Gutierrez 			17909
+ * @author Raul Monzon					17014
+ * @since 26 de enero 2018
+ */
+public interface CalculadoraI {
+	public String calcular(String expresion);
+}
