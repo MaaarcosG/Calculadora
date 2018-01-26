3181e72c973030c772f0c9a122cbb7ae924972e5
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-01-26 11:52:09 -0600

    Intento Soy hueco RaulM

--- src/CalculadoraI.java
+++ src/CalculadoraI.java
@@ -1,8 +1,9 @@
 /**
  * @author Marcos Gutierrez 			17909
  * @author Raul Monzon					17014
- * @since 26 de enero 2018
+ * @since 26 de enero 201
  */
 public interface CalculadoraI {
+	// Soy bien hueco
 	public String calcular(String expresion);
 }
