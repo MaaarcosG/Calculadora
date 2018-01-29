78fd33ae277b34a5bbe2dd859218adba33ff2b3a
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-29 17:21:28 -0600

    Solo falta realizar las operaciones

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -11,28 +11,16 @@ import java.io.InputStreamReader;
 public class Calculadora implements CalculadoraI {
 	/*Atributos de la clase*/
 	private static Stack<Integer> datos;
-	private String line1;
-	private String line2;
-	private int resultado;
-	private int num1;
-	private int num2;
 	
-	/*Constructor de la clase*/
-	public Calculadora() {
-		line1 = "";
-		line2 = "";
-		resultado = 0;
-		num1 = 0;
-		num2 = 0;
-	}
 	@Override
 	public String calcular(String expresion) {
+
 		return expresion;
 	}
 	/**
 	 * Este metodo sirve para abrir el documento tipo texto
 	 */
-	public void abrirArchivo() {
+	public void abrirArchivo(String line1, String expresion) {
 		try {
 			/*Abrimos el archivo de texto*/
 			FileInputStream archivo = new FileInputStream("datos.txt");
@@ -43,7 +31,7 @@ public class Calculadora implements CalculadoraI {
 			/*Leer archivo*/
 			while ((line1 = buffer.readLine()) != null) {
 				/*Imprime linea*/
-				line2 = line1;
+				expresion = line1;
 				System.out.println("Operaciones ingresada " + line1);
 			}
 			entrada.close();
--- src/Main_Prueba.java
+++ src/Main_Prueba.java
@@ -1,9 +1,13 @@
-
+import javax.swing.JOptionPane;
 
 public class Main_Prueba {
+	private static Stack<Integer> datos;
 	public static void main(String [] args) {
-		Calculadora leer = new Calculadora();
-		leer.abrirArchivo();
 		
+		Calculadora leer = new Calculadora();
+		String line1 = "";
+		String expresion = "";
+		leer.abrirArchivo(line1, expresion);
+		System.out.println("El resultado es de: " + leer.calcular(expresion));
 	}
 }
