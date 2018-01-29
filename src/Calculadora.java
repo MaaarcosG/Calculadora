be0936ed45494ab1fdd91b7016c1fcd0c5a18b37
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-29 14:18:01 -0600

    Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,27 +9,36 @@ import java.io.InputStreamReader;
  * @since 26 de enero 2018
  */
 public class Calculadora implements CalculadoraI {
+	/*Atributos de la clase*/
+	private static Stack<Integer> datos;
+	private String line1;
+	private String line2;
+	private int resultado;
+	private int num1;
+	private int num2;
+	
 	/*Constructor de la clase*/
 	public Calculadora() {
-		
+		line1 = "";
+		line2 = "";
+		resultado = 0;
+		num1 = 0;
+		num2 = 0;
 	}
 	@Override
 	public String calcular(String expresion) {
-		// TODO Auto-generated method stub
-		return null;
+		return expresion;
 	}
 	/**
-	 * 
+	 * Este metodo sirve para abrir el documento tipo texto
 	 */
 	public void abrirArchivo() {
-		String line1 = "";
-		String line2 = "";
 		try {
 			/*Abrimos el archivo de texto*/
 			FileInputStream archivo = new FileInputStream("datos.txt");
 			/*Objeto de entrada*/
 			DataInputStream entrada = new DataInputStream(archivo);
-			/*Buffer, que seriva para lectura*/
+			/*Buffer, que servira para lectura*/
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
 			/*Leer archivo*/
 			while ((line1 = buffer.readLine()) != null) {
