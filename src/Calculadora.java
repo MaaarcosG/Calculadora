91581ca061e76c068a38eb3734ab72836578fd40
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-29 13:34:32 -0600

    Se añadieron nuevas clase

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,7 +3,6 @@
  * @author Raul Monzon					17014
  * @since 26 de enero 2018
  */
-public interface CalculadoraI {
-	// Soy bien hueco
-	public String calcular(String expresion);
+public class Calculadora {
+
 }
