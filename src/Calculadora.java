510422b70f57f4540dea4429579d2ac67ef46a2d
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-29 13:53:29 -0600

    Actualizada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,8 +1,18 @@
+
 /**
  * @author Marcos Gutierrez 			17909
  * @author Raul Monzon					17014
  * @since 26 de enero 2018
  */
-public class Calculadora {
-
+public class Calculadora implements CalculadoraI {
+	/*Constructor de la clase*/
+	public Calculadora() {
+		
+	}
+	@Override
+	public String calcular(String expresion) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- src/Main_Prueba.java
+++ src/Main_Prueba.java
@@ -1,4 +1,20 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Main_Prueba {
-
+	public static void main(String [] args) {
+		try {
+			/*Abrimos el archivo de texto*/
+			FileInputStream archivo = new FileInputStream("datos.txt");
+			/*Objeto de entrada*/
+			DataInputStream entrada = new DataInputStream(archivo);
+			/*Buffer, que seriva para lectura*/
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+		} catch (Exception e) {
+			/**/
+			System.err.println("Ocurrio un error" + e.getMessage());
+		}
+	}
 }
