896eb4b18cb7c76efca8e1d587512476f4ad263f
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-01-30 13:45:53 -0600

    Corrigiendo errores

--- src/Main_Prueba.java
+++ src/Main_Prueba.java
@@ -1,35 +1,39 @@
 import javax.swing.JOptionPane;
+import java.io.*;
 
 public class Main_Prueba {
+	
 	private static Stack<Integer> dato = new StackArrayList<Integer>(); 
+	
 	public static void main(String [] args) {
 		
 		Calculadora leer = new Calculadora();
 		
 		try {
-			
+		
 			/*Abrimos el archivo de texto*/
 			FileInputStream archivo = new FileInputStream("datos.txt");
 			/*Objeto de entrada*/
 			DataInputStream entrada = new DataInputStream(archivo);
 			/*Buffer, que servira para lectura*/
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-			
-			
 			/*Leer archivo*/
-			while ((line1 = buffer.readLine()) != null) {
-				/*Imprime linea*/
-				expresion = line1;
-				System.out.println("Operaciones ingresada " + line1);
-			}
 			
+			while (buffer.readLine() != null) {
+				String line=buffer.readLine();
+				System.out.println(line);
+				System.out.println("estado 1");
+			}
+			System.out.println("estado 2");
 			entrada.close();
 		
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			/*Mensaje de error*/
 			System.err.println("Ocurrio un error " + e.getMessage());
+			System.out.println("estado 3");
 		}
 		
-		System.out.println("El resultado es de: " + dato.pop());
+		//System.out.println("El resultado es de: " + dato.pop());
 	}
 }
