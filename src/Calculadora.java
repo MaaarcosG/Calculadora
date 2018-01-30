d052cddbff3ef6fb7ba6b828768a7c712fda8071
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-01-30 13:32:39 -0600

    ola k ase? haciendo cambios o k ase?
    Solo cambie de ubicacion el método que abre el archivo y cambie el .txt
    de datos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -9,39 +9,20 @@ import java.io.InputStreamReader;
  * @since 26 de enero 2018
  */
 public class Calculadora implements CalculadoraI {
+	
 	/*Atributos de la clase*/
 	private static Stack<Integer> datos = new StackArrayList<Integer>();
 	
-	 /*Constructor de la clase*/
+	
+	/*Constructor de la clase*/
 	public Calculadora() {
 		
 	}
+	
 	@Override
 	public String calcular(String expresion) {
 	
 		return expresion;
 	}
-	/**
-	 * Este metodo sirve para abrir el documento tipo texto
-	 */
-	public void abrirArchivo(String line1, String expresion) {
-		try {
-			/*Abrimos el archivo de texto*/
-			FileInputStream archivo = new FileInputStream("datos.txt");
-			/*Objeto de entrada*/
-			DataInputStream entrada = new DataInputStream(archivo);
-			/*Buffer, que servira para lectura*/
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-			/*Leer archivo*/
-			while ((line1 = buffer.readLine()) != null) {
-				/*Imprime linea*/
-				expresion = line1;
-				System.out.println("Operaciones ingresada " + line1);
-			}
-			entrada.close();
-		} catch (Exception e) {
-			/*Mensaje de error*/
-			System.err.println("Ocurrio un error " + e.getMessage());
-		}
-	}
+	
 }
--- src/Main_Prueba.java
+++ src/Main_Prueba.java
@@ -5,9 +5,31 @@ public class Main_Prueba {
 	public static void main(String [] args) {
 		
 		Calculadora leer = new Calculadora();
-		String line1 = "";
-		String expresion = "";
-		leer.abrirArchivo(line1, expresion);
+		
+		try {
+			
+			/*Abrimos el archivo de texto*/
+			FileInputStream archivo = new FileInputStream("datos.txt");
+			/*Objeto de entrada*/
+			DataInputStream entrada = new DataInputStream(archivo);
+			/*Buffer, que servira para lectura*/
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+			
+			
+			/*Leer archivo*/
+			while ((line1 = buffer.readLine()) != null) {
+				/*Imprime linea*/
+				expresion = line1;
+				System.out.println("Operaciones ingresada " + line1);
+			}
+			
+			entrada.close();
+		
+		} catch (Exception e) {
+			/*Mensaje de error*/
+			System.err.println("Ocurrio un error " + e.getMessage());
+		}
+		
 		System.out.println("El resultado es de: " + dato.pop());
 	}
 }
