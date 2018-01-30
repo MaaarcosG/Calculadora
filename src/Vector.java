6f075d2900fc93cc505177d6547c6d366d12b9a1
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-30 16:10:55 -0600

    Clase Vector

---
+++ src/Vector.java
@@ -0,0 +1,40 @@
+
+public class Vector<E> implements Stack<E> {
+	protected Vector<E> datos;
+	
+	/*Constructor de la clase*/
+	public Vector() {
+		datos = new Vector<E>();
+	}
+	
+	@Override
+	public void push(E item) {
+		
+		
+	}
+
+	@Override
+	public E pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
