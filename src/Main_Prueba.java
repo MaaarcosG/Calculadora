205b76a43c3ba77181780fe025be92e3dfa96d96
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-30 14:09:52 -0600

    Corregi el error

--- src/Main_Prueba.java
+++ src/Main_Prueba.java
@@ -1,35 +1,42 @@
 import javax.swing.JOptionPane;
+import java.io.*;
 
 public class Main_Prueba {
+	
 	private static Stack<Integer> dato = new StackArrayList<Integer>(); 
+	
 	public static void main(String [] args) {
-		
+		String linea1 = "";
+		String linea2 = "";
 		Calculadora leer = new Calculadora();
 		
 		try {
-			
+		
 			/*Abrimos el archivo de texto*/
 			FileInputStream archivo = new FileInputStream("datos.txt");
 			/*Objeto de entrada*/
 			DataInputStream entrada = new DataInputStream(archivo);
 			/*Buffer, que servira para lectura*/
 			BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
-			
-			
 			/*Leer archivo*/
-			while ((line1 = buffer.readLine()) != null) {
-				/*Imprime linea*/
-				expresion = line1;
-				System.out.println("Operaciones ingresada " + line1);
+			linea1 = buffer.readLine();
+			if (linea1 == null) {
+				System.err.println("El .txt esta vacio");
+			}
+			else {
+				linea2 = linea1;
+				System.out.println("Lo que esta en el archivo del texto: " + linea2);
 			}
 			
 			entrada.close();
 		
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			/*Mensaje de error*/
 			System.err.println("Ocurrio un error " + e.getMessage());
+			System.out.println("estado 2");
 		}
 		
-		System.out.println("El resultado es de: " + dato.pop());
+		//System.out.println("El resultado es de: " + dato.pop());
 	}
 }
