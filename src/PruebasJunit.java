918b87576db152ca92bddf6ff7c6215c7b8695b4
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-02-02 16:45:06 -0600

    TODO TERMINADO

--- src/PruebasJunit.java
+++ src/PruebasJunit.java
@@ -3,9 +3,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PruebasJunit {
-
+	/*Prueba de JUnit*/
 	@Test
 	void pruebaStack() {
+		/*Atributos de la clas stack*/
 		StackVector<String> datos = new StackVector<String>();
 		/* Prueba Push */
 		datos.push("1");
@@ -16,7 +17,7 @@ class PruebasJunit {
 		datos.pop();
 		assertEquals(true, datos.empty());
 	}
-	
+	/*Prueba de la calculadora*/
 	@Test
 	void pruebaCalc() {
 		Calculadora calcu = new Calculadora();
